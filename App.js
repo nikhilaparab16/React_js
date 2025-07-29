6ec6af769285a311a5f9a780eab0c5b9d92d5e6a
Nikhila <nikhilaprab16@gmail.com>
2025-07-29 12:08:40 +0530

    28th July 2025

--- App.js
+++ App.js
@@ -1,51 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-<div class="root">
-    <div class="parent">
-        <div class="child">
-            <h1></h1>
-            <h2></h2>
-        </div> 
+const title = <span>React App</span>
+const HeaderComponent = () =>(
+    <h1>This is header</h1>
+)
+
+
+const HeadingComponent = () =>(
+    <div className="container">
+        <HeaderComponent></HeaderComponent>
+        {HeaderComponent()}
+        <h2 className="heading">Hello world from Functional Component</h2>
     </div>
-</div>
-
-
-
-*/
-
-
-// const heading = React.createElement(
-//     "h1",
-//     {id:"heading", xyz:"title"},
-//     "Hello World From React"
-// );
-//console.log(heading) //its an object ..its not tag , react element 
-
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement(
-            "div",
-            {id:"child"},
-            [
-                React.createElement("h1",{},"I am H1 tag"),
-                React.createElement("h2",{},"I am h2 tag")
-            ]
-        ),
-        React.createElement(
-            "div",
-            {id:"child2"},
-            [
-                React.createElement("h1",{},"I am H1 tag"),
-                React.createElement("h2",{},"I am h2 tag")
-            ]
-        )
-    ]
-);
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent)
+root.render(<HeadingComponent />)
